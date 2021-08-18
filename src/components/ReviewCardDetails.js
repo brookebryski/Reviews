f05658b248c676dda2acecb49655ab0bb2949bf8
github username <github email address>
2021-08-18 10:38:21 -0500

    reviewcarddetails

--- src/components/ReviewCardDetails.js
+++ src/components/ReviewCardDetails.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import { reviews } from '../data';
-import StarRatings from 'react-star-ratings';
-import './Review.css';  
+import React from "react";
+import { useLocation } from "react-router-dom";
+import StarRatings from "react-star-ratings";
 
- const ReviewCard = ({ review }) => {
-  return (  
-    <div className="card-deck"> 
-    {reviews.map((review) => {
-        return (
-         <div className="card">
-          <div key={review.id}>
-            <h4 className="card-title">{review.place}</h4>
-            <StarRatings
-                rating={review.rating}
-                starRatedColor="gold" 
-                starDimension="20px"
-                />
-            <div className="card-body">{review.content}</div>
-            <div className="card-footer">{review.author} - {review.published_at}</div> 
-            
+const ReviewCardDetails = () => {
+  const location = useLocation();
+  const { review } = location?.state; // ? - optional chaining
+
+  console.log("history location details: ", location);
+  return (
+    <div key={review.id} className="card-deck">
+      <div className="card">
+        <div>
+          <h4 className="card-title">{review.place}</h4>
+          <StarRatings
+            rating={review.rating}
+            starRatedColor="gold"
+            starDimension="20px"
+          />
+          <div className="card-body">{review.content}</div>
+          <div className="card-footer">
+            {review.author} - {review.published_at}
+          </div>
+        </div>
       </div>
-      </div>
-        );
-         })}
-      </div> 
-          );
-      };
-  
-export default ReviewCard;
+    </div>
+  );
+};
+
+export default ReviewCardDetails;
