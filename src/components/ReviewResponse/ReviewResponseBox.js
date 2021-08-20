ca4b26243688d4bba88c3437202aa64de4323349
github username <github email address>
2021-08-20 16:28:37 -0700

    implemented localstorage in reviewresponsebox

--- src/components/ReviewResponse/ReviewResponseBox.js
+++ src/components/ReviewResponse/ReviewResponseBox.js
@@ -1,43 +1,37 @@
-import React from 'react';
-import ReviewResponse from './ReviewResponse';
-import ReviewResponseForm from './ReviewResponseForm';
-import { reactLocalStorage } from "reactjs-localstorage";
+import React from "react";
+import ReviewResponse from "./ReviewResponse";
+import ReviewResponseForm from "./ReviewResponseForm";
 
 class ReviewResponseBox extends React.Component {
-  constructor() {
-    super()
+  state = {
+    reviewResponses: JSON.parse(localStorage.getItem("reviewResponses")) || []
+  };
 
-    this.state = {
-      reviewResponses: []
-    };
- }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.reviewResponses !== this.state.reviewResponses) {
+      localStorage.setItem(
+        "reviewResponses",
+        JSON.stringify(this.state.reviewResponses)
+      );
+    }
+  }
 
- //fetchResponses = () => {
- // let reviewResponses = [];
-//  localStorage.setResponses
-//  reviewResponses.push(reviewResponse);
-//}
-  
-  render () {
+  render() {
     const reviewResponses = this.getResponses();
-    const reviewResponseNodes = <div className="reviewResponse-list">{reviewResponses}</div>;
-   
-    return(
-      <div className="reviewResponse-box">
-        {reviewResponses.length 
-          ? (
-            <>
-              {reviewResponseNodes}
-            </>
-          )
-          : (
-            <ReviewResponseForm addResponse={this.addResponse.bind(this)}/>
-          )}
-      </div> 
-        
+    const reviewResponseNodes = (
+      <div className="reviewResponse-list">{reviewResponses}</div>
     );
-  } 
-  
+
+    return (
+      <div className="reviewResponse-box">
+        {reviewResponses.length ? (
+          <>{reviewResponseNodes}</>
+        ) : (
+          <ReviewResponseForm addResponse={this.addResponse.bind(this)} />
+        )}
+      </div>
+    );
+  }
 
   addResponse(review_id, author, body) {
     const reviewResponse = {
@@ -45,23 +39,21 @@ class ReviewResponseBox extends React.Component {
       author,
       body
     };
-    this.setState({ reviewResponses: this.state.reviewResponses.concat([reviewResponse]) }); // *new array references help React stay fast, so concat works better than push here.
-  }
-   
-  
-  
-  getResponses() {    
-    return this.state.reviewResponses.map((reviewResponse) => { 
-      return (
-        <ReviewResponse 
-          author={reviewResponse.author} 
-          body={reviewResponse.body} 
-          review_id={this.state.review_id} />
-      ); 
-    });
+    this.setState({
+      reviewResponses: this.state.reviewResponses.concat([reviewResponse])
+    }); // *new array references help React stay fast, so concat works better than push here.
   }
 
-  
-  
+  getResponses() {
+    return this.state.reviewResponses.map((reviewResponse) => {
+      return (
+        <ReviewResponse
+          author={reviewResponse.author}
+          body={reviewResponse.body}
+          review_id={this.state.review_id}
+        />
+      );
+    });
+  }
 }
 export default ReviewResponseBox;
