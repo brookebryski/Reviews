bc05fbd0af8b30f996addcd844d38e461d4fc630
github username <github email address>
2021-08-21 14:41:32 -0700

    css background and header background

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import  ReviewCollection  from './components/Review/ReviewCollection';
 import  ReviewCardDetails  from './components/Review/ReviewCardDetails';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
  
-document.body.setAttribute('style', 'background: grey;')
+document.body.setAttribute('style', 'background: 	#A0A0A0;')
 
 class App extends React.Component {
   render() {
