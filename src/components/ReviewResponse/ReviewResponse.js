79c34e44bc587391dbc04c07704d93431338f210
github username <github email address>
2021-08-20 17:16:26 -0700

    edited css for .card so they are all the same size

--- src/components/ReviewResponse/ReviewResponse.js
+++ src/components/ReviewResponse/ReviewResponse.js
@@ -6,8 +6,9 @@ class ReviewResponse extends React.Component {
       return(
         <div className="response">
           <i class="reply icon" style={{ color: 'blue'}}></i>
-          <p className="response-header">{this.props.author}</p>
-          <p className="response-body">- {this.props.body}</p>
+          <br></br>
+          <p className="response-header">{this.props.body}</p>
+          <p className="response-body">- {this.props.author}</p>
           <div className="response-footer" >
             </div>
         </div>
