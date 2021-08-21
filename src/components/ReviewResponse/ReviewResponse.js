97764a9fc23f223771eac312c07282fece459438
github username <github email address>
2021-08-21 12:59:13 -0700

    ellipsis icon formating

--- src/components/ReviewResponse/ReviewResponse.js
+++ src/components/ReviewResponse/ReviewResponse.js
@@ -10,18 +10,23 @@ class ReviewResponse extends React.Component {
 
     render () {
       return(
-        <div className="response">
+        <div className="response"> 
+        <span class="reply-icon">
           <i class="reply icon" style={{ color: 'blue'}}></i>
+          </span>
+          <span class="ellipsis">
+           <i class="ellipsis horizontal icon" style={{ color: 'blue' }}></i>
+           </span>
           <br></br>
           <p className="response-header">{this.props.body}</p>
           <p className="response-body">- {this.props.author}</p>
           <div className="response-footer" >
-          <button class="ui button" onClick={() => this.handleClick()}>Edit</button>
+          
             </div>
         </div>
       );
     }
-  
+
   }
   export default ReviewResponse;
   
