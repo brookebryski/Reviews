e2586d45953ed91e39f92ddb86609a86905daddc
github username <github email address>
2021-08-22 22:24:50 -0700

    edit functionality

--- src/components/ReviewResponse/ReviewResponse.js
+++ src/components/ReviewResponse/ReviewResponse.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import './ReviewResponse.css';
-// import ReviewResponseForm from "./ReviewResponseForm";
+ import ReviewResponseForm from "./ReviewResponseForm";
 
 class ReviewResponse extends React.Component {
- 
- // handleClick() {
-  // return <ReviewResponseForm />
- //   }
+  // I struggled with trying to implement the 'edit' functionality 
+
+  // My idea was to set the initial state as a boolean, 'formOpen', and set it to false. This way when the edit icon is clicked the component re-renders, state is set to formOpen: true and the ReviewResponseForm renders
+ state = {
+  formOpen: false
+ };
+
+ handleClick() {
+  this.setState({formOpen: true && <ReviewResponseForm />})
+  console.log('click')}
+
     render () {
       return(
         <div className="response">
           <span class="ellipsis">
-           <i class="ellipsis horizontal icon" style={{ color: 'blue' }}></i>
+           <i class="ellipsis horizontal icon" onClick={() => this.handleClick()} style={{ color: 'blue' }}>
+           </i>
            </span>
            <br></br>
            <span class="reply-icon">
